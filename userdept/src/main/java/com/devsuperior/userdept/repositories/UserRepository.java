e54548fec642aee87af6e455c9668bdb83241686
Yudy <106198975+yudyAS@users.noreply.github.com>
2024-12-22 15:01:34 -0300

    Delete userdept directory

--- userdept/src/main/java/com/devsuperior/userdept/repositories/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.devsuperior.userdept.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.devsuperior.userdept.entities.User;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-
-}
